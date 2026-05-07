fa99528ce50c3cc10ca9effc0fe3a254520f1641
Christian König <christian.koenig@amd.com>
2026-05-07 14:55:35 +0200

    drm/amdgpu: add gfp_flags to amdgpu_sa_manager v2
    Make sure that we use the emmergency reserves for unrecoverable page
    faults and GPU resets.
    
    v2: improve code comments a bit based on Timur's feedback
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ib.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ib.c
@@ -356,6 +356,25 @@ int amdgpu_ib_pool_init(struct amdgpu_device *adev)
 		[AMDGPU_IB_POOL_IMMEDIATE] = SZ_128K,
 		[AMDGPU_IB_POOL_DIRECT] = SZ_512K
 	};
+	const gfp_t gfp_flags[AMDGPU_IB_POOL_MAX] = {
+		/*
+		 * For normal page table updates and recoverable retry faults
+		 * (for SVM), further restricted by the VM eviction lock to not
+		 * wait for memory reclaim.
+		 */
+		[AMDGPU_IB_POOL_DELAYED] = GFP_KERNEL,
+		/*
+		 * For redirecting unrecoverable retry faults to the dummy page
+		 * or set the PRT bits. dma_fence submissions might depend on
+		 * that so we need the emmergency reserves.
+		 */
+		[AMDGPU_IB_POOL_IMMEDIATE] = GFP_ATOMIC,
+		/*
+		 * For IB tests during GPU resets. Only very small and temporary
+		 * allocation to allow dma_fences to signal.
+		 */
+		[AMDGPU_IB_POOL_DIRECT] = GFP_ATOMIC
+	};
 	int r, i;
 
 	if (adev->ib_pool_ready)
@@ -363,8 +382,7 @@ int amdgpu_ib_pool_init(struct amdgpu_device *adev)
 
 	for (i = 0; i < AMDGPU_IB_POOL_MAX; i++) {
 		r = amdgpu_sa_bo_manager_init(adev, &adev->ib_pools[i],
-					      sizes[i], 256,
-					      AMDGPU_GEM_DOMAIN_GTT);
+					      sizes[i], gfp_flags[i]);
 		if (r)
 			goto error;
 	}
