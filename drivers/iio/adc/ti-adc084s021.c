2b26ccd8869278a4975dccda0b77798aca286470
Prashant Rahul <prashantrahul23@gmail.com>
2026-06-19 04:44:35 +0530

    iio: adc: ti-adc084s021: use dev_err_probe for probe time error
    This simplifies error handling and ensures consistent error reporting.
    
    Signed-off-by: Prashant Rahul <prashantrahul23@gmail.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/adc/ti-adc084s021.c
+++ drivers/iio/adc/ti-adc084s021.c
@@ -229,10 +229,8 @@ static int adc084s021_probe(struct spi_device *spi)
 	ret = devm_iio_triggered_buffer_setup(&spi->dev, indio_dev, NULL,
 					    adc084s021_buffer_trigger_handler,
 					    &adc084s021_buffer_setup_ops);
-	if (ret) {
-		dev_err(&spi->dev, "Failed to setup triggered buffer\n");
-		return ret;
-	}
+	if (ret)
+		return dev_err_probe(&spi->dev, ret, "Failed to setup triggered buffer\n");
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
 }
