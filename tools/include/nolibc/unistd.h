94518b77e194d71b1d76a2dd75f4891954edc296
Daniel Palmer <daniel@thingy.jp>
2026-07-02 17:51:00 +0900

    tools/nolibc: unistd: Add readlink()
    Add readlink(). This is needed to test getcwd().
    
    Signed-off-by: Daniel Palmer <daniel@thingy.jp>
    Acked-by: Willy Tarreau <w@1wt.eu>
    Link: https://patch.msgid.link/20260702085101.3304547-3-daniel@thingy.jp
    Signed-off-by: Thomas Weißschuh <linux@weissschuh.net>

--- tools/include/nolibc/unistd.h
+++ tools/include/nolibc/unistd.h
@@ -128,6 +128,22 @@ int msleep(unsigned int msecs)
 		return 0;
 }
 
+/*
+ * ssize_t readlink(const char *path, char *buf, size_t bufsiz);
+ */
+
+static __attribute__((unused))
+ssize_t _sys_readlink(const char *path, char *buf, size_t bufsiz)
+{
+	return __nolibc_syscall4(__NR_readlinkat, AT_FDCWD, path, buf, bufsiz);
+}
+
+static __attribute__((unused))
+ssize_t readlink(const char *path, char *buf, size_t bufsiz)
+{
+	return __sysret(_sys_readlink(path, buf, bufsiz));
+}
+
 static __attribute__((unused))
 unsigned int sleep(unsigned int seconds)
 {
