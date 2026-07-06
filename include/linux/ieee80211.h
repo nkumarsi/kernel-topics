ac798f757d6475dc6fee2ec899980d6740714596
Ilan Peer <ilan.peer@intel.com>
2026-07-06 22:29:31 +0300

    wifi: mac80211: Route (Re)association req/response to per-STA queue
    An association request or response frame is generally delivered to the
    driver without a TX queue object. However, with drivers that do encryption
    offload and couple the key with a transmit queue, this means that the
    frames are not being encrypted.
    
    Fix this by routing the association frames to the management TXQ.
    This will allow the driver to set up the required resources before
    transmitting the association frame, e.g., set up keys etc.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Link: https://patch.msgid.link/20260706222925.febcc62f485c.Iff932880e4b98232cbf6ba405fbb90d650a85381@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- include/linux/ieee80211.h
+++ include/linux/ieee80211.h
@@ -557,6 +557,17 @@ static inline bool ieee80211_is_reassoc_resp(__le16 fc)
 }
 
 /**
+ * ieee80211_is_assoc - check if (Re)association request/response frame
+ * @fc: frame control bytes in little-endian byteorder
+ * Return: whether or not the frame is an (re)association request or response
+ */
+static inline bool ieee80211_is_assoc(__le16 fc)
+{
+	return ieee80211_is_assoc_req(fc) || ieee80211_is_reassoc_req(fc) ||
+	       ieee80211_is_assoc_resp(fc) || ieee80211_is_reassoc_resp(fc);
+}
+
+/**
  * ieee80211_is_probe_req - check if IEEE80211_FTYPE_MGMT && IEEE80211_STYPE_PROBE_REQ
  * @fc: frame control bytes in little-endian byteorder
  * Return: whether or not the frame is a probe request
