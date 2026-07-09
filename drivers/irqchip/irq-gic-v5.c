5a64611747687189415a5298a16e1ad4c08a829b
Lorenzo Pieralisi <lpieralisi@kernel.org>
2026-07-09 10:40:16 +0200

    ACPI/IORT: Implement ACPI infrastructure to enable GICv5 IWB probe deferral
    Implement an IORT ACPI hook to retrieve the acpi_handle of the interrupt
    controller handling a specific GSI (if any, on GICv5 systems only the IWB
    is represented in firmware with an ACPI device object) and add the IWB to
    the list of devices whose dependencies can be detected (and cleared) in
    ACPI core to guarantee that probe dependencies for the IWB can be
    satisfied.
    
    Enable autodep detection for arm64 by adding the arch_acpi_add_auto_dep()
    callback in the ACPI IORT driver.
    
    Signed-off-by: Lorenzo Pieralisi <lpieralisi@kernel.org>
    Link: https://patch.msgid.link/20260709-gic-v5-acpi-iwb-probe-deferral-v4-6-48dae790f871@kernel.org
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/irqchip/irq-gic-v5.c
+++ drivers/irqchip/irq-gic-v5.c
@@ -1226,7 +1226,7 @@ static struct fwnode_handle *gsi_domain_handle;
 static struct fwnode_handle *gic_v5_get_gsi_domain_id(u32 gsi)
 {
 	if (FIELD_GET(GICV5_GSI_IC_TYPE, gsi) == GICV5_GSI_IWB_TYPE)
-		return iort_iwb_handle(FIELD_GET(GICV5_GSI_IWB_FRAME_ID, gsi));
+		return iort_iwb_handle_fwnode(FIELD_GET(GICV5_GSI_IWB_FRAME_ID, gsi));
 
 	return gsi_domain_handle;
 }
