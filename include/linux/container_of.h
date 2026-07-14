f810b41defc44535715afa978dbcba8b3b9b9d99
Vincent Mailhol <mailhol@kernel.org>
2026-07-14 20:18:01 +0200

    container_of: apply typeof_member() to container_of()
    container_of() uses the construct below:
    
    	((type *)0)->member
    
    to retrieve the type of the structure's member and then ensure that it
    matches the type of the given pointer.
    
    This construct being rather difficult to understand, the typeof_member()
    macro was created to encapsulate it and give it a descriptive name.
    
    Apply typeof_member() to container_of() to make it easier to read and
    understand what this macro does.
    
    Signed-off-by: Vincent Mailhol <mailhol@kernel.org>
    Link: https://patch.msgid.link/20260714-containerof_refactor-v1-1-b5c31164d2ad@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/container_of.h
+++ include/linux/container_of.h
@@ -18,7 +18,7 @@
  */
 #define container_of(ptr, type, member) ({				\
 	void *__mptr = (void *)(ptr);					\
-	static_assert(__same_type(*(ptr), ((type *)0)->member) ||	\
+	static_assert(__same_type(*(ptr), typeof_member(type, member)) || \
 		      __same_type(*(ptr), void),			\
 		      "pointer type mismatch in container_of()");	\
 	((type *)(__mptr - offsetof(type, member))); })
